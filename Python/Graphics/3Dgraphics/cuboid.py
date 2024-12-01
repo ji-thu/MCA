88096be1fd4ad4ff60ac0e3dc8fee39138b431cb
ji-thu <139971564+ji-thu@users.noreply.github.com>
2024-12-01 10:17:01 +0530

    Add files via upload

---
+++ Python/Graphics/3Dgraphics/cuboid.py
@@ -0,0 +1,4 @@
+def area(length, breadth, height):
+    return 2 * (length * breadth + length * height + breadth * height)
+def perimeter(length, breadth, height):
+    return 4 * (length + breadth + height)
---
+++ Python/Graphics/3Dgraphics/sphere.py
@@ -0,0 +1,5 @@
+import math
+def area(radius):
+    return 4 * math.pi * radius * radius
+def perimeter(radius):
+    return 2 * math.pi * radius
